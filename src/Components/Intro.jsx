3257d36de14bf838218da8ad2e57958e453e8c4d
Steve <scampbellatc@gmail.com>
2023-08-07 22:28:30 -0400

    sudo code for components

---
+++ src/Components/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Intro(){
+    return (
+
+        // TODO: Basic intro page will go here with button to open quiz app
+
+        //BASIC H1 TITLE
+        // H4 description of quiz if needed
+        // Button to start quiz (this will hold the fetch for the API, hide the intro screen, and display the Quiz component)
+        <>
+        
+       
+
+        </>
+    )
+}
