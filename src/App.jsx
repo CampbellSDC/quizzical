017669a2bd9446efa9c459ec490ce7c371cc0f63
Steve <scampbellatc@gmail.com>
2023-08-08 13:02:57 -0400

    incomplete quiz state

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,37 @@
 import React from 'react'
 import Intro from './Components/Intro'
+import Quiz from './Components/Questions'
 import './App.css'
 
 function App() {
-  const [quizQuestions, setQuizQuestions] = React.useState()
+  const [quizQuestions, setQuizQuestions] = React.useState(getQuiz())
+
   //TODO: Now that quizQuestions state is established, this will be used in the quiz component
   //TODO: to map over the quizQuestions array, and create a variable that holds 
   //TODO: the 10 questions that are retrieved from the fetch. This will be done
   //TODO: by assigning quizQuestions as a prop for the questions component.
+
+  //* Need to figure out how to set quizQuestions state to questions from API fetch
+
+console.log(quizQuestions)
 
   function getQuiz() {
     fetch("https://opentdb.com/api.php?amount=10&category=17&difficulty=medium&type=multiple")
     .then(res => res.json())
     .then(data => {
       const questions = data.results
-      setQuizQuestions(questions)
+      return questions
     })
     
 }
+
+
+
+
+
+// const newQuestions = quizQuestions.map(question => {
+//   console.log(question.results)
+// }) 
 
 
   return (
@@ -25,6 +39,8 @@ function App() {
       <Intro
         handleClick={getQuiz}
       />
+
+      
     </>
   )
 }
